d1b9b41de93a4639eac3c1d238285a375adfc192
masamichi.sato <masamichi.sato@FVFHQ6JEQ05N.local>
2023-12-27 07:26:52 +0900

    class Question

--- main.py
+++ main.py
@@ -4,57 +4,53 @@ import sys
 Q_NUM = 10
 OPT = ["A", "B", "C", "D"]
 
+class Question:
+    q_num = 10
+    opt = ["A", "B", "C", "D"]
+    challenge_cnt = 0
+    passed = False
 
-def random_correct_answers() -> list:
-    return [OPT[random.randint(0, 3)] for i in range(Q_NUM)]
+    def __init__(self) -> None:
+        self.ans = [self.opt[random.randint(0, 3)] for i in range(self.q_num)]
 
-def validate_ans(ans: list) -> bool:
-    if len(ans) != Q_NUM:
-        return False
-    for a in ans:
-        if not a in OPT:
-            return False
-    return True
-
-def input_ans() -> list:
-    ans = list(input().replace(" ", ""))
-    valid = validate_ans(ans)
-    while not valid:
-        print("your answers are not valid")
+    def answer(self) -> list:
         ans = list(input().replace(" ", ""))
-        valid = validate_ans(ans)
-    return ans
+        while not self.validate_ans(ans):
+            print("your answers are not valid")
+            ans = list(input().replace(" ", ""))
+        self.challenge_cnt += 1
+        score = self.score(ans)
+        print(f"your score is {score}")
+        self.set_passed(score)
+        return score
 
-def count_correct_answers(l1: list, l2: list) -> int:
-    return sum(1 for i in range(Q_NUM) if l1[i] == l2[i])
+    def set_passed(self, score: int) -> None:
+        self.passed = (self.q_num == score)
+        if self.passed:
+            print(f"You passed the test on your {self.challenge_cnt} challenge!")
 
-def main(debug: bool = False) -> None:
-    correct_ans = random_correct_answers()
-    challenge_cnt = 1
-    if debug:
-        print(f"charange {challenge_cnt}")
-    user_ans = input_ans()
-    correct_cnt = count_correct_answers(correct_ans, user_ans)
-    passed = (correct_cnt == Q_NUM)
-    while not passed:
-        print(f"your score is {correct_cnt}")
-        if debug:
-            print("debug" + "-" * 20)
-            print(f"your answers\t{user_ans}")
-            print(f"correct answers\t{correct_ans}")
-            print("debug" + "-" * 20)
-            print(f"charange {challenge_cnt}")
-        challenge_cnt += 1
-        user_ans = input_ans()
-        correct_cnt = count_correct_answers(correct_ans, user_ans)
-        passed = (correct_cnt == Q_NUM)
-        
-    print(f"You passed the test on your {challenge_cnt} challenge!")
+    def validate_ans(self, ans: list | None) -> bool:
+        if not ans:
+            return False
+        if len(ans) != self.q_num:
+            return False
+        for a in ans:
+            if not a in self.opt:
+                return False
+        return True
+
+    def score(self, ans: list) -> int:
+        return sum(1 for i in range(self.q_num) if ans[i] == self.ans[i])
+
+
+def main() -> None:
+    question = Question()
+    try:
+        while not question.passed:
+            score = question.answer()
+    except KeyboardInterrupt:
+        print(f"\ncorrect answer is {''.join(question.ans)}")
 
 
 if __name__ == '__main__':
-    try:
-        debug = bool(len(sys.argv) > 1 and bool(sys.argv[1]))
-        main(debug)
-    except KeyboardInterrupt:
-        pass
+    main()
